cc8d2dda578049427242fdb37468ad2284c75594
Critic Team <Critic Team@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-23 23:38:13 +0000

    Deleted all plugin files
    git-svn-id: https://plugins.svn.wordpress.org/games/trunk@1074497 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/global-settings/page-header.php
+++
@@ -1,7 +0,0 @@
-
-
-<div class="wgames-logo-view" style="padding: 15px;">
-    <img src="<?php echo $setting_logo_path?>" style="display: none; width:304px; margin-top: 20px;margin-left: 10px;">
-     <h2 style="color: #428bca">Critic Games</h2>
-</div>
-
